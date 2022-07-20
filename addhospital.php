d489c257de69c6001a22722f6bd57a3c801f3535
Nobab khan <75307851+Nobabkh@users.noreply.github.com>
2022-07-20 21:02:33 +0600

    hospital

--- addhospital.php
+++ addhospital.php
@@ -9,14 +9,14 @@ $host = "localhost";
 $con = mysqli_connect($host, $user, $pass, $dp);
 if($con)
 {
-    $res = $con->query("SELECT * FROM location WHERE district = '$dist' AND sub_dist = '$subdist';");
+    $res = $con->query("SELECT * FROM location WHERE district = '$dist' AND sub_dist = '$subdist' AND hospital = '$hospital';");
     if(mysqli_num_rows($res) > 0)
     {
-        $res1 = $con->query("INSERT INTO location SET hospital = '$hospital' WHERE ");
+        $res1 = $con->query("UPDATE location SET hospital = '$hospital' WHERE district = '$dist' AND sub_dist = '$subdist';");
     }
     else
     {
-
+        $res2 = $con->query("INSERT INTO location (district, sub_dist, hospital) VALUES ('$dist', '$subdist', '$hospital');");
     }
    
 }
